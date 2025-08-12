1672733f587a87a4f3f2bacbfbf3e9f9e073d010
Shreyas2545 <shreyashadawale25@gmail.com>
2025-08-12 10:57:20 +0530

    another commit

--- hertz_Frontend/src/main.jsx
+++ hertz_Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store.js";
-
+import React from "react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
